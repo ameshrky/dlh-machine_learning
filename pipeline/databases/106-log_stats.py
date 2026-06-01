426681d41c2f6ee898cedd62404a002cf73180d8
Antwan Meshrky <antoinemagdi@gmail.com>
2026-06-01 22:01:31 +0200

    Fix 106 log stats

--- pipeline/databases/106-log_stats.py
+++ pipeline/databases/106-log_stats.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
-"""Provide statistics about Nginx logs stored in MongoDB."""
+"""Print Nginx log statistics stored in MongoDB."""
 from pymongo import MongoClient
 
-if name == "main":
-collection = MongoClient("mongodb://127.0.0.1:27017").logs.nginx
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
-print("{} logs".format(collection.count_documents({})))
-print("Methods:")
-for method in methods:
-    print("\tmethod {}: {}".format(
-        method, collection.count_documents({"method": method})))
-print("{} status check".format(
-    collection.count_documents({"method": "GET", "path": "/status"})))
-print("IPs:")
-for doc in collection.aggregate([
-    {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-    {"$sort": {"count": -1}},
-    {"$limit": 10},
-]):
-    print("\t{}: {}".format(doc.get("_id"), doc.get("count")))
+if __name__ == "__main__":
+    collection = MongoClient("mongodb://127.0.0.1:27017").logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
+    print("{} logs".format(collection.count_documents({})))
+    print("Methods:")
+    for method in methods:
+        print("\tmethod {}: {}".format(method, collection.count_documents({"method": method})))
+    print("{} status check".format(collection.count_documents({"method": "GET", "path": "/status"})))
+    print("IPs:")
+    for doc in collection.aggregate([{"$group": {"_id": "$ip", "count": {"$sum": 1}}}, {"$sort": {"count": -1}}, {"$limit": 10}]):
+        print("\t{}: {}".format(doc["_id"], doc["count"]))
