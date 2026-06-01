46f5704c2e36606709ec1e866580f68630d0d26c
Antwan Meshrky <antoinemagdi@gmail.com>
2026-06-01 22:00:09 +0200

    Fix 105 students

--- pipeline/databases/105-students.py
+++ pipeline/databases/105-students.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Return students sorted by average score."""
 
+
 def top_students(mongo_collection):
-"""Return all students sorted by average score."""
-return mongo_collection.aggregate([
-{"$addFields": {"averageScore": {"$avg": "$topics.score"}}},
-{"$sort": {"averageScore": -1}},
-])
+    """Return all students sorted by average score."""
+    return mongo_collection.aggregate([
+        {"$addFields": {"averageScore": {"$avg": "$topics.score"}}},
+        {"$sort": {"averageScore": -1}}
+    ])
