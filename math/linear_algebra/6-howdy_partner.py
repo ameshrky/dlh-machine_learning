176ea6c4f113343d21a053fe00a85191df6f67d7
Antwan Meshrky <antwan.meshrky@ai.dlh.lu>
2026-06-15 00:51:30 +0200

    task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that adds arrays element-wise."""
+
+
+def cat_arrays(arr1, arr2):
+    """Returns the element-wise addition of two arrays."""
+
+    return arr1 + arr2
