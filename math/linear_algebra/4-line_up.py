d8913520945acf88f7c1de0ebc7f89b83469164b
Antwan Meshrky <antwan.meshrky@ai.dlh.lu>
2026-06-15 00:45:03 +0200

    task 4

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-"""Module that transposes a matrix."""
+"""Module that adds arrays element-wise."""
 
 
 def add_arrays(arr1, arr2):
-    """Returns the transpose of a 2D matrix."""
+    """Returns the element-wise addition of two arrays."""
 
     arr1_len = len(arr1)
     arr2_len = len(arr2)
 
     if arr1_len != arr2_len:
         return None
-    
+
     new_array = []
 
     for c in range(arr1_len):
-            new_array.append(arr1[c] + arr2[c])
-    
+        new_array.append(arr1[c] + arr2[c])
+
     return new_array
